74173b817643d526d028b9c399236f05007b8122
IstoMan <suhasnaraparaju@gmail.com>
2026-06-29 15:36:33 +0530

    had input field

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,31 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Calcutor'), centerTitle: true),
-        body: MainBoard(),
+        body: Column(
+          mainAxisAlignment: .end,
+          children: [Display(), MainBoard()],
+        ),
       ),
       theme: ThemeData.dark(),
+    );
+  }
+}
+
+class Display extends StatelessWidget {
+  const Display({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: '0',
+          labelStyle: TextStyle(fontSize: 40, fontWeight: FontWeight.w600),
+        ),
+        style: TextStyle(fontSize: 40, fontWeight: FontWeight.w600),
+      ),
     );
   }
 }
@@ -43,6 +65,14 @@ class MainBoard extends StatelessWidget {
               DigitButton(symbol: '8', type: DigitType.number),
               DigitButton(symbol: '9', type: DigitType.number),
               DigitButton(symbol: '×', type: DigitType.operator),
+            ],
+          ),
+          Row(
+            children: [
+              DigitButton(symbol: '4', type: DigitType.number),
+              DigitButton(symbol: '5', type: DigitType.number),
+              DigitButton(symbol: '6', type: DigitType.number),
+              DigitButton(symbol: '+', type: DigitType.operator),
             ],
           ),
           Row(
@@ -84,7 +114,7 @@ class DigitButton extends StatelessWidget {
       margin: const EdgeInsets.all(2),
       decoration: BoxDecoration(
         color: switch (type) {
-          DigitType.number => Colors.grey,
+          DigitType.number => Colors.black,
           DigitType.operator => Colors.orange,
           DigitType.equal => Colors.blue,
           DigitType.back => Colors.yellow.shade700,
